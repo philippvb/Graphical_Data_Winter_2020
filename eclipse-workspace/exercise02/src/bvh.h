e5c76fd3057ea15b94961d327c701f0773e8adf4
philippvb <philippvonbachmann@web.de>
2020-11-19 17:32:26 +0100

    Added assignment 2

---
+++ eclipse-workspace/exercise02/src/bvh.h
@@ -0,0 +1,100 @@
+#ifndef BVH_H
+#define BVH_H
+
+#include "rtStructs.h"
+
+/**
+ * One node of the BVH tree.
+ * @remarks a BVH tree node has either child nodes or triangles, never both!
+ */
+struct Node
+{
+	/// Includes either all its child nodes or all its vertices.
+	AABB bbox;
+	/**
+	 * Index of the left child node in the BVH::nodes array.
+	 * @remarks Should be -1 in leaf nodes.
+	 */
+	int left;
+	/**
+	 * Index of the right child node in the BVH::nodes array.
+	 * @remarks Should be -1 in leaf nodes.
+	 */
+	int right;
+
+	/**
+	 * A Node contains all the triangles referenced by BVH::indices' elements
+	 * triIndex to triIndex + numTris - 1.
+	 * @remarks Should be -1 if not a leaf node.
+	 */
+	int triIndex;
+	/**
+	 * Number of triangles of this node.
+	 * @remarks Should be 0 if not a leaf node.
+	 */
+	int numTris;
+};
+
+struct BVH
+{
+	/// Array of all triangles indexed by the BVH (access indirectly via indices!)
+	const Triangle * const tris;
+	/// Number of triangles.
+	const int nTris;
+
+	// this is the bvh!
+	/// Array containing all nodes of the BVH.
+	Node *nodes;
+	/// Amount of nodes that have already been added (is incremented while building the tree).
+	int addedNodes;
+	/// Used for indirectly accessing the tris. indices[i] contains the index of a triangle in tris.
+	int *indices;
+
+	/**
+	 * Builds a BVH tree for a set of triangles.
+	 * @param tris Array of triangles.
+	 * @param nTris Number if triangles in tris.
+	 */
+	BVH(const Triangle * const tris, const int nTris);
+	/**
+	 * Default deconstructor.
+	 */
+	~BVH();
+
+	/**
+	 * Finds a good split plane.
+	 * @param dimension Out parameter: Returns the splitted dimension.
+	 * @param position Out parameter: Returns the split position in dimension.
+	 * This value should be exclusive in the lower and inclusive in the higher
+	 * node.
+	 * @param box Describes the volume to be split.
+	 */
+	void findSplitPlane(unsigned int* dimension, float* position,
+			const AABB &box);
+
+	/**
+	 * Builds a BVH tree out of a set of triangles in tris.
+	 * @remarks: Intended to call itself recursively for each subnode.
+	 * @param nodeIndex Root node index of the subtree that is processed
+	 * during the call.
+	 * @param triIndex First element of indices referencing a triangle that
+	 * belongs to the current node.
+	 * @param numTris Number of triangles belonging to the current node.
+	 * @param depth Recursion depth = tree depth of the node currently
+	 * processed.
+	 */
+	void buildBVH(int nodeIndex, int triIndex, int numTris, int depth);
+
+	/**
+	 * Finds the closest intersection of a ray and all the triangles in the
+	 * BVH efficiently.
+	 * @ray Ray to intersect with the BVH's triangles.
+	 * @returns Hit record of the closest intersection between triangle and ray.
+	 */
+	HitRec intersect(const Ray &ray) const;
+
+	// add extra methods if needed (i.e. sortTriangles(...))
+};
+
+#endif
+
---
+++ eclipse-workspace/exercise02/src/main.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <fstream>
+#include <omp.h>
+#include "cam.h"
+#include "rtStructs.h"
+#include "bvh.h"
+#include "utils/vec.h"
+#include "utils/fileio.h"
+
+PerspCam *cam;
+
+#ifdef INTERACTIVE
+#include <SDL/SDL.h>
+#include <SDL_opengl.h>
+
+bool finished = false;
+
+SDL_Surface *screen;
+void initScreen(int ResX, int ResY)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_SetVideoMode(ResX, ResY, 32, SDL_OPENGL | SDL_GL_DOUBLEBUFFER);
+}
+
+void eventHandling()
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				finished = true;
+				break;
+			case SDLK_q:
+				finished = true;
+				break;
+			case SDLK_w:
+				break;
+			case SDLK_a:
+				break;
+			case SDLK_s:
+				break;
+			case SDLK_d:
+				break;
+			default:
+				break;
+			}
+			break;
+		case SDL_KEYUP:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_w:
+				break;
+			case SDLK_a:
+				break;
+			case SDLK_s:
+				break;
+			case SDLK_d:
+				break;
+			default:
+				break;
+			}
+			break;
+		case SDL_MOUSEMOTION:
+			if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(1))
+			{
+				break;
+			}
+			break;
+		case SDL_QUIT:
+			finished = true;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+#endif
+
+/// Width of the rendered image.
+int ResX = 300;
+/// Height of the rendered image.
+int ResY = 300;
+
+/**
+ * Buffer containing the rendered images.
+ * @remarks Pixels are stored rowwise, so pixel positions can be calculated by
+ * y*ResX+x.
+ */
+Vec3 *buffer;
+/// BVH tree used for accelerating the rendering.
+BVH *bvh;
+
+//Triangle *tris; data is now in the bvh structure
+//int num_tris;
+
+/**
+ * Renders an image of ResX * ResY size into buffer.
+ */
+void render()
+{
+	for (int y = 0; y < ResY; y++)
+	{
+		for (int x = 0; x < ResX; x++)
+		{
+			Ray ray = cam->getRay(x, y);
+
+			HitRec rec = bvh->intersect(ray);
+
+			if (rec.id != -1)
+			{
+				Vec3 normal = bvh->tris[rec.id].getNormal();
+				buffer[x + y * ResX] = fabsf(ray.dir * normal);
+			}
+			else
+				buffer[x + y * ResX] = Vec3(0.0f, 0.0f, 0.0f);
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	float *data;
+	int num_data;
+
+	if (argc == 2)
+		num_data = load_float_data(argv[1], data);
+	else
+		num_data = load_float_data("Test.ra2", data);
+
+	Triangle *tris = (Triangle*) data;
+	int num_tris = num_data / 9;
+
+	std::cout << "#Triangles " << num_tris << std::endl;
+
+	bvh = new BVH(tris, num_tris);
+
+	buffer = new Vec3[ResX * ResY];
+
+	cam = new PerspCam(Vec3(0.0f, 0.0f, 120.0f), Vec3(0.0f, 0.0f, -1.0f),
+			Vec3(0.0f, 1.0f, 0.0f), ResX, ResX, ResY);
+
+#ifdef INTERACTIVE
+	initScreen(ResX, ResY);
+	char title[256];
+	float fps = 0.0f;
+	int frame = 0;
+	unsigned int t0 = SDL_GetTicks();
+
+	while (!finished)
+	{
+		eventHandling();
+
+		render();
+
+		glDrawPixels(ResX, ResY, GL_RGB, GL_FLOAT, (float*) buffer);
+		SDL_GL_SwapBuffers();
+		sprintf(title, "%g fps", fps);
+		SDL_WM_SetCaption(title, NULL);
+		unsigned int t1 = SDL_GetTicks();
+		frame++;
+		if (t1 - t0 > 500.0f)
+		{
+			fps = 1000.0f / (t1 - t0) * frame;
+			t0 = t1;
+			frame = 0;
+		}
+	}
+#else
+	render();
+#endif
+	save_image_ppm("image.ppm", (float*) buffer, ResX, ResY);
+
+	return 0;
+}
+
